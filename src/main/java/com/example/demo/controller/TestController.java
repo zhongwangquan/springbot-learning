4e9b574f10287af20f9b5600feaaca08eac6961f
1813114234@qq.com <1813114234@qq.com>
2024-01-10 13:06:02 +0800

    feat: 尝试连接数据库

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,11 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.MyData;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@SpringBootApplication
 @RequestMapping("/test")
 public class TestController {
 
